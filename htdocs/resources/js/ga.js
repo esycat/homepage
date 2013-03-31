d9c40ad006443492307409ed69c2b6565a2e0578
Eugene Janusov <esycat@gmail.com>
2013-04-01 00:27:12 +1100

    Updated Google Analytics tracking code.

--- htdocs/resources/js/ga.js
+++ htdocs/resources/js/ga.js
@@ -1,9 +1,11 @@
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    var gaAccountId = 121822;
+    var gaTrackerId = 'UA-' + gaAccountId + '-4';
+    var gaDomain = 'esyfur.com';
 
-try {
-	var pageTracker = _gat._getTracker("UA-121822-3");
-	pageTracker._trackPageview();
-	alert(3);
-}
-catch (exception) {}
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', gaTrackerId, gaDomain);
+  ga('send', 'pageview');
